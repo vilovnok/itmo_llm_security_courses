0b08da454110f82e26f9911f1c61592fa612249b
vilovnok <vilovnok@gmail.com>
2025-03-14 21:17:32 +0300

    add test red teaming

--- agent/vllm/run.py
+++ agent/vllm/run.py
@@ -13,7 +13,7 @@ def main(cfg: DictConfig):
     
     command = [
         "python", "-m", "vllm.entrypoints.openai.api_server",
-        "--model", cfg.model.deepseek,
+        "--model", cfg.model.giga,
         "--gpu-memory-utilization", str(cfg.model.gpu_memory_utilization),
         "--port", str(cfg.model.portV2)
     ]
