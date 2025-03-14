14c02f2fca43dff1e1e713655ed3ab21af08cbcd
vilovnok <vilovnok@gmail.com>
2025-03-14 13:39:39 +0300

    For OpenAI Client

---
+++ agent/openai-client/run.py
@@ -0,0 +1,23 @@
+import hydra
+from omegaconf import DictConfig
+
+import subprocess
+
+from huggingface_hub import login
+
+
+@hydra.main(version_base=None, config_path="../../", config_name="config")
+def main(cfg: DictConfig):        
+    
+    login(token=cfg.model.hf_token)
+    
+    command = [
+        "python", "-m", "vllm.entrypoints.openai.api_server",
+        "--model", cfg.model.base_model,
+        "--gpu-memory-utilization", str(cfg.model.gpu_memory_utilization),
+        "--port", str(cfg.model.port)
+    ]
+    subprocess.run(command)
+
+if __name__ == "__main__":
+    main()
